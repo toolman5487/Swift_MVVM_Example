03f9617ba4135bf0f63494f62c184f6fc14f4425
Willy <willy548798@gmail.com>
2025-03-02 22:31:14 +0800

    建立 FriendPageViewController Unit Test

---
+++ Swift_MVVMTests/Swift_MVVMTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_MVVMTests.swift
+//  Swift_MVVMTests
+//
+//  Created by Willy Hsu on 2025/3/2.
+//
+
+import Testing
+
+struct Swift_MVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
